e991b3588b2bf505b3e11b73e032580163d7cafd
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-15 16:46:34 +0530

    Refactor introduce method to check value of grade

--- src/main/java/com/tw/student/Student.java
+++ src/main/java/com/tw/student/Student.java
@@ -4,19 +4,21 @@ import com.tw.student.exceptions.NegativeGradeException;
 
 public class Student {
     private final IWritingStrategy writeWith;
+    private final int grade;
 
     public Student(int grade) throws NegativeGradeException {
-        if (grade < 0) throw new NegativeGradeException();
-        if (grade <= 2) {
-            writeWith = new SlatePencilStrategy();
-        } else if (grade <= 5) {
-            writeWith = new PencilStrategy();
-        } else {
-            writeWith = new PenStrategy();
-        }
+        this.grade = grade;
+        if (isGradeLessThanOrEqualTo(-1)) throw new NegativeGradeException();
+        if (isGradeLessThanOrEqualTo(2)) writeWith = new SlatePencilStrategy();
+        else if (isGradeLessThanOrEqualTo(5)) writeWith = new PencilStrategy();
+        else writeWith = new PenStrategy();
     }
 
     public String write() {
         return writeWith.write();
+    }
+
+    private boolean isGradeLessThanOrEqualTo(int number) {
+        return grade <= number;
     }
 }
